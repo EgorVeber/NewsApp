4b7f7bb3be1bd6d6279ceaadac708a2a4f110ad6
EgorVeber <99020910+EgorVeber@users.noreply.github.com>
2023-02-16 20:41:10 +0300

    NewsAndroid-22 Fix sprint3. Удалить ChangeRequestModule (#27)

--- app/src/main/java/ru/gb/veber/newsapi/view/activity/ActivityMain.kt
+++ app/src/main/java/ru/gb/veber/newsapi/view/activity/ActivityMain.kt
@@ -2,8 +2,6 @@ package ru.gb.veber.newsapi.view.activity
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.RecyclerView
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import io.reactivex.rxjava3.core.Completable
@@ -14,13 +12,12 @@ import ru.gb.veber.newsapi.core.App
 import ru.gb.veber.newsapi.databinding.ActivityMainBinding
 import ru.gb.veber.newsapi.model.SharedPreferenceAccount
 import ru.gb.veber.newsapi.presenter.ActivityPresenter
-import ru.gb.veber.newsapi.utils.*
 import ru.gb.veber.newsapi.utils.ACCOUNT_LOGIN_DEFAULT
 import ru.gb.veber.newsapi.utils.COUNTER_BADGE
 import ru.gb.veber.newsapi.utils.COUNTER_BACKSTACK
+import ru.gb.veber.newsapi.utils.ColorUtils.getDrawableRes
 import ru.gb.veber.newsapi.utils.DELAY_BACK_STACK
 import ru.gb.veber.newsapi.utils.showText
-import ru.gb.veber.newsapi.view.profile.account.settings.CustomizeCategoryFragment
 import ru.gb.veber.newsapi.view.profile.account.settings.EditAccountFragment
 import ru.gb.veber.newsapi.view.search.searchnews.SearchNewsFragment
 import ru.gb.veber.newsapi.view.topnews.pageritem.EventBehaviorToActivity
@@ -179,13 +176,13 @@ class ActivityMain : MvpAppCompatActivity(), ViewMain, OpenScreen, EventLogoutAc
     override fun bottomNavigationSetDefaultIcon() {
         var item = binding.bottomNavigationView.menu.findItem(R.id.actionProfile)
         item.title = ACCOUNT_LOGIN_DEFAULT
-        item.icon = ContextCompat.getDrawable(this, R.drawable.ic_baseline_person_add_alt_1_24)
+        item.icon = this.getDrawableRes(R.drawable.ic_baseline_person_add_alt_1_24)
     }
 
     override fun onCreateSetIconTitleAccount(accountLogin: String) {
         var item = binding.bottomNavigationView.menu.findItem(R.id.actionProfile)
         item.title = accountLogin
-        item.icon = ContextCompat.getDrawable(this, R.drawable.ic_baseline_person_24)
+        item.icon = this.getDrawableRes(R.drawable.ic_baseline_person_24)
     }
 
     override fun completableInsertSources() {
@@ -195,7 +192,7 @@ class ActivityMain : MvpAppCompatActivity(), ViewMain, OpenScreen, EventLogoutAc
     override fun bottomNavigationSetCurrentAccount(checkLogin: String) {
         var item = binding.bottomNavigationView.menu.findItem(R.id.actionProfile)
         item.title = checkLogin
-        item.icon = ContextCompat.getDrawable(this, R.drawable.ic_baseline_person_24)
+        item.icon = this.getDrawableRes(R.drawable.ic_baseline_person_24)
     }
 
     override fun bottomNavigationSetTitleCurrentAccount(checkLogin: String) {
