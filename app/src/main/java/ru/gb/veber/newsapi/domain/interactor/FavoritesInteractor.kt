2679e2abc6c6a65ffc68c04e8308a79fb1d4def2
EgorKisel <81646532+EgorKisel@users.noreply.github.com>
2023-03-17 18:51:12 +0300

    NewsAndroid-67 FavoritesFragment Добавить Интерактор (#76)

---
+++ app/src/main/java/ru/gb/veber/newsapi/domain/interactor/FavoritesInteractor.kt
@@ -0,0 +1,30 @@
+package ru.gb.veber.newsapi.domain.interactor
+
+import ru.gb.veber.newsapi.data.models.room.entity.ArticleDbEntity
+import ru.gb.veber.newsapi.domain.repository.ArticleRepo
+import javax.inject.Inject
+
+class FavoritesInteractor @Inject constructor(
+    private val articleRepoImpl: ArticleRepo
+) {
+
+    suspend fun getLikeArticle(accountId: Int): List<ArticleDbEntity> {
+        return articleRepoImpl.getLikeArticleByIdV2(accountId)
+    }
+
+    suspend fun getHistoryArticle(accountId: Int): List<ArticleDbEntity> {
+        return articleRepoImpl.getHistoryArticleByIdV2(accountId)
+    }
+
+    suspend fun deleteArticleFavorites(title: String, accountId: Int) {
+        articleRepoImpl.deleteArticleByIdFavoritesV2(title, accountId)
+    }
+
+    suspend fun deleteArticleHistory(title: String, accountId: Int) {
+        articleRepoImpl.deleteArticleByIdHistoryV2(title, accountId)
+    }
+
+    suspend fun deleteArticleHistoryGroup(accountId: Int,dateAdded:String) {
+        articleRepoImpl.deleteArticleByIdHistoryGroupV2(accountId, dateAdded)
+    }
+}
