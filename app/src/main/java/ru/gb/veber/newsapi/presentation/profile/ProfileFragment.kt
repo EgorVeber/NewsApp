82b893a6f5ece58be2a63ef9acd81871e39bac92
lachesis-lab <mgmmail@mail.ru>
2023-03-21 18:44:24 +0300

    NewsAndroid-81 ProfileFragment удалить viewModel

--- app/src/main/java/ru/gb/veber/newsapi/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/ru/gb/veber/newsapi/presentation/profile/ProfileFragment.kt
@@ -2,7 +2,9 @@ package ru.gb.veber.newsapi.presentation.profile
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.github.terrakok.cicerone.Router
+import ru.gb.veber.newsapi.common.screen.AccountScreen
+import ru.gb.veber.newsapi.common.screen.AuthorizationScreen
 import ru.gb.veber.newsapi.common.utils.ACCOUNT_ID
 import ru.gb.veber.newsapi.common.utils.BundleInt
 import ru.gb.veber.newsapi.core.App
@@ -11,24 +13,19 @@ import javax.inject.Inject
 class ProfileFragment : Fragment() {
 
     @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
+    lateinit var router: Router
 
     private var accountId by BundleInt(ACCOUNT_ID,0)
-
-    private val profileViewModel by lazy {
-        ViewModelProvider(this, viewModelFactory)[ProfileViewModel::class.java]
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         App.instance.appComponent.inject(this)
-        var accountId = arguments?.getInt(ACCOUNT_ID) ?: 0
 
         if (accountId != 0) {
-            profileViewModel.openScreenProfile(accountId)
+            router.replaceScreen(AccountScreen(accountId))
         } else {
-            profileViewModel.openScreenAuthorization()
+            router.replaceScreen(AuthorizationScreen)
         }
     }
 
