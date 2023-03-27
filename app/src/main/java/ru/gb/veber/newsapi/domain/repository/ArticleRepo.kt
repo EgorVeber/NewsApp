ae41205bad8acfae837e2243e127e83046c73c75
lachesis-lab <mgmmail@mail.ru>
2023-03-27 14:05:28 +0300

    NewsAndroid-82 ArticleRepoImpl. Убрать RX, возвращать в domain слой domain модели.  Убрать лишние мапперы в presentation.

--- app/src/main/java/ru/gb/veber/newsapi/domain/repository/ArticleRepo.kt
+++ app/src/main/java/ru/gb/veber/newsapi/domain/repository/ArticleRepo.kt
@@ -1,42 +1,15 @@
 package ru.gb.veber.newsapi.domain.repository
 
-import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Single
-import ru.gb.veber.newsapi.data.models.room.entity.ArticleDbEntity
 import ru.gb.veber.newsapi.domain.models.Article
 
 interface ArticleRepo {
-/*
-    fun insertArticle(articleDbEntity: ArticleDbEntity): Completable
-    fun updateArticle(articleDbEntity: ArticleDbEntity): Completable
-    fun deleteArticle(articleDbEntity: ArticleDbEntity): Completable
-    fun deleteAllArticle(): Completable
-    fun getHistoryArticleById(accountId: Int): Single<List<ArticleDbEntity>>
-    fun getLikeArticleById(accountId: Int): Single<List<ArticleDbEntity>>
-    fun getLastArticle(): Single<ArticleDbEntity>
-    fun getArticleById(accountId: Int): Single<List<ArticleDbEntity>>
-    fun deleteArticleIsFavoriteById(accountId: Int): Completable
-    fun deleteArticleIsHistoryById(accountId: Int): Completable
-    fun deleteArticleByIdFavorites(title: String, accountId: Int): Completable
-    fun deleteArticleByIdHistory(title: String, accountId: Int): Completable
-    fun deleteArticleByIdHistoryGroup( accountId: Int,dateAdded:String): Completable
-
-*/
-
     suspend fun insertArticleV2(article: Article, accountId: Int)
-//    suspend fun insertArticleV2(articleDbEntity: ArticleDbEntity)
     suspend fun updateArticleV2(article: Article, accountId: Int)
-    //suspend fun updateArticleV2(articleDbEntity: ArticleDbEntity)
-//    suspend fun deleteArticleV2(articleDbEntity: ArticleDbEntity)
     suspend fun deleteArticleV2(article: Article, accountId: Int)
     suspend fun deleteAllArticleV2()
-//    suspend fun getHistoryArticleByIdV2(accountId: Int): List<ArticleDbEntity>
     suspend fun getHistoryArticleByIdV2(accountId: Int): List<Article>
-    //suspend fun getLikeArticleByIdV2(accountId: Int): List<ArticleDbEntity>
     suspend fun getLikeArticleByIdV2(accountId: Int): List<Article>
     suspend fun getLastArticleV2(): Article
-    //suspend fun getLastArticleV2(): ArticleDbEntity
-    //suspend fun getArticleByIdV2(accountId: Int): List<ArticleDbEntity>
     suspend fun getArticleByIdV2(accountId: Int): List<Article>
     suspend fun deleteArticleIsFavoriteByIdV2(accountId: Int)
     suspend fun deleteArticleIsHistoryByIdV2(accountId: Int)
