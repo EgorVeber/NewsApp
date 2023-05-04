0d63d440876576d2be1d4a552c54a64e8c1d1df4
JamesOhara <egor.veber.02.07.95@gmail.com>
2023-05-05 00:33:58 +0300

    Сделать динамическое получение иконок для примера.

--- app/src/main/java/ru/gb/veber/newsapi/presentation/keymanagement/KeysManagementFragment.kt
+++ app/src/main/java/ru/gb/veber/newsapi/presentation/keymanagement/KeysManagementFragment.kt
@@ -1,9 +1,19 @@
 package ru.gb.veber.newsapi.presentation.keymanagement
 
 import KeysDialogFragment
+import android.R.attr.bottom
+import android.R.attr.left
+import android.R.attr.right
+import android.R.attr.top
+import android.graphics.drawable.Drawable
 import android.transition.TransitionManager
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.setFragmentResultListener
 import com.github.terrakok.cicerone.Router
+import ru.gb.veber.newsapi.R
 import ru.gb.veber.newsapi.common.base.NewsFragment
 import ru.gb.veber.newsapi.common.extentions.hide
 import ru.gb.veber.newsapi.common.extentions.observeFlow
@@ -18,7 +28,9 @@ import ru.gb.veber.newsapi.databinding.KeysManagementFragmentBinding
 import ru.gb.veber.newsapi.domain.models.ApiKeysModel
 import ru.gb.veber.newsapi.presentation.keymanagement.recycler.KeysListener
 import ru.gb.veber.newsapi.presentation.keymanagement.recycler.KeysManagementAdapter
+import java.lang.reflect.Field
 import javax.inject.Inject
+
 
 class KeysManagementFragment :
     NewsFragment<KeysManagementFragmentBinding, KeysManagementViewModel>(
@@ -61,6 +73,64 @@ class KeysManagementFragment :
         binding.goToSiteB.setOnClickListener {
             router.navigateTo(WebViewScreen(PAGE_GET_KEY))
         }
+
+        val drawablesFields: Array<Field> = R.drawable::class.java.fields
+//        val drawables: ArrayList<Drawable> = ArrayList()
+//
+//        for (field in drawablesFields) {
+//            try {
+//               // drawables.add(resources.getDrawable(field.getInt(null)))
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//        }
+
+
+//        drawablesFields.filter { it.name != it.name.substringBefore("ic_") }
+//            .filter { it.name != it.name.substringBefore("baseline") }.forEach {
+//            Log.d("filter", it.name)
+//        }
+
+
+//       drawablesFields.filter { it.name ==it.name.substringBefore("ic_").substringBefore("baseline") && it.name == it.name.substringBefore("baseline")}
+//           .forEach {
+//               Log.d("forEach", it.name)
+//            }
+
+        drawablesFields.filter { it.name ==it.name.substringBefore("ic_").substringBefore("baseline") }
+            .forEach {
+                Log.d("forEach", it.name)
+            }
+
+        val id: Int = context?.resources?.getIdentifier("drawable/" + drawablesFields[7].name,
+            null,
+            requireContext().packageName) ?: R.drawable.news_api_logo
+        val id2: Int = context?.resources?.getIdentifier("drawable/" + drawablesFields[8].name,
+            null,
+            requireContext().packageName) ?: R.drawable.news_api_logo
+        val id3: Int = context?.resources?.getIdentifier("drawable/" + drawablesFields[9].name,
+            null,
+            requireContext().packageName) ?: R.drawable.news_api_logo
+
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        params.setMargins(40, 20, 20, 20)
+
+        binding.lineaLayout.addView(ImageView(context).apply {
+            setImageResource(id)
+            layoutParams = params
+        })
+        binding.lineaLayout.addView(ImageView(context).apply {
+            setImageResource(id2)
+            layoutParams = params
+        })
+        binding.lineaLayout.addView(ImageView(context).apply {
+            setImageResource(id3)
+            layoutParams = params
+        })
     }
 
     override fun onObserveData() {
